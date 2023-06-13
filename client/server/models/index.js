6956e8fc500a07ee1da95e3bf3038ed65f75a93b
tykervella <kervellatyler@gmail.com>
2023-06-13 15:16:55 -0700

    added homepage

--- client/server/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./User');
-const Deck = require('./Deck');
-const Listing = require('./Listing');
-const Post = require('./Post');
-
-
-module.exports = { User, Deck, Listing, Post };
